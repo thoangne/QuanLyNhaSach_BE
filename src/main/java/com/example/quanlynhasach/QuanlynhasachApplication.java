8f2fc661d15f11e295dfc05cc2a8590301b7c973
Minh Tuấn <75988895+MinhTuan120704@users.noreply.github.com>
2025-05-21 14:29:54 +0700

    Create project

---
+++ src/main/java/com/example/quanlynhasach/QuanlynhasachApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlynhasach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlynhasachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlynhasachApplication.class, args);
+	}
+
+}
